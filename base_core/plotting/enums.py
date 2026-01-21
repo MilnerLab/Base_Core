34d233d207779bf490ea0605029aaae0d3e55429
MilnerLab <milnerlab@protonmail.com>
2026-01-21 14:09:37 -0800

    colors added

--- base_core/plotting/enums.py
+++ base_core/plotting/enums.py
@@ -7,4 +7,13 @@ class PlotColor(str, Enum):
     GREEN  = "g"
     PURPLE = "purple"
     GRAY   = "tab:gray"
+    BLACK = "k"
+    
+class PlotColorMap(str, Enum):
+    DEFAULT = "viridis"
+    PLASMA = "plasma"
+    INFERNO = "inferno"
+    MAGMA = "magma"
+    CIVIDIS = "cividis"
+    
     
