999323fe7148d2aeccadad1250a66d2dbf4d61e0
MilnerLab <milnerlab@protonmail.com>
2026-04-09 12:59:54 -0700

    small error fixed

--- base_core/quantities/molecules.py
+++ base_core/quantities/molecules.py
@@ -279,6 +279,6 @@ class OCS(Molecule):
                 ),
                 bond_axis=CartesianAxis.Z, #I-I axis
                 aniso = PolarizabilityVolume.from_angstrom3(3.7)),
-            spinnability=(),
+            
             tags=("heavy", "droplets"),
         )
