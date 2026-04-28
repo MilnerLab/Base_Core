c59d576e69e249ecd356ae4df714227fd7f9719d
MilnerLab <milnerlab@protonmail.com>
2026-04-27 18:38:19 -0700

    fixed cut function

--- base_core/lab_specifics/base_models.py
+++ base_core/lab_specifics/base_models.py
@@ -33,14 +33,14 @@ class ScanDataBase:
                     w.writerow([d, m.value, m.error])
             print("Data saved to:",path)
             
-    def cut(self, start: int = 0, end: int = 0) -> None:
+    def cut(self, start: int = 0, end: int = -1) -> None:
         n = len(self.delays)
-        if len(self.measured_values) != n or start < 0 or end < 0 or start + end > n:
+        if len(self.measured_values) != n or start < 0 or end < 0 or abs(start - end) < 2:
             raise ValueError("Invalid cut range.")
 
         if end:
-            del self.delays[-end:]
-            del self.measured_values[-end:]
+            del self.delays[end+1:]
+            del self.measured_values[end+1:]
         if start:
             del self.delays[:start]
             del self.measured_values[:start]
